635f39e10b7ad9894f8ea1eb48b6d589e11a4b8c
Sypchik <tastysypchik@proton.me>
2025-02-20 16:26:19 +0300

    update

--- main.tf
+++ main.tf
@@ -595,11 +595,11 @@ resource "local_file" "kuber_init_conf" {
 resource "null_resource" "ansible_apply" {
   provisioner "local-exec" {
     command = <<EOT
-      ansible-playbook -vvvv -i ${path.module}/ansible/inventory/hosts.yaml ${path.module}/ansible/playbooks/site.yaml -e "ansible_ssh_extra_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
+      ansible-playbook -v -i ${path.module}/ansible/inventory/hosts.yaml ${path.module}/ansible/playbooks/site.yaml
     EOT
 
     environment = {
-      ANSIBLE_HOST_KEY_CHECKING = false
+      ANSIBLE_HOST_KEY_CHECKING = "False"
       ANSIBLE_CONFIG            = "${path.module}/ansible/ansible.cfg"
     }
   }
